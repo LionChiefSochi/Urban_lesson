c906300c103132dcd0e5b9289e0abb8508d64abb
LionChiefSochi <gridnev_ilya@mail.ru>
2024-12-30 13:59:43 +0300

    Add files via upload

---
+++ Module4.1.py
@@ -0,0 +1,12 @@
+from fake_math import *
+from true_math import *
+
+
+result1 = f_divide(69, 3)
+result2 = f_divide(3, 0)
+result3 = divide(49, 7)
+result4 = divide(15, 0)
+print(result1)
+print(result2)
+print(result3)
+print(result4)
